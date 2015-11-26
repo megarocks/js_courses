76c45b6f61c4448278c8057fc912144ce04abb79
Stanislav <Stas@nesklada.com>
2015-11-26 18:38:30 +0200

    Home

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -1,1 +1,9 @@
 //add your code here
+var search = document.getElementById('stas-nesklada');
+search.className += ('success');
+
+function stringColor(){
+	var styling = document.getElementsByClassName('succes');
+	styling.style.background = "red";
+	}
+setTimeout(stringColor,8000);
