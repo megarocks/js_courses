9773ef8fed73462e1cb7bbdf5bdc60775e1a0834
AnnaKarpenko <anna.lopanchuk@gmail.com>
2015-11-26 18:53:23 +0300

    новая правка

--- class_06/anna-karpenko/hw_5/homework.js
+++ class_06/anna-karpenko/hw_5/homework.js
@@ -1,4 +1,4 @@
- var date;
+﻿ var date;
  var dateArray = [];
  function putIntoArray() { 
  	date = document.getElementsByTagName('input')[0].value;
@@ -26,4 +26,3 @@ function showMaxAndMinDate() {
 	}
 	document.getElementsByTagName('p')[0].innerHTML = "Наибольшая дата: " + maxDate;
 	document.getElementsByTagName('p')[1].innerHTML = "Наименьшая дата: " + minDate;
-}
