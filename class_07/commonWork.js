b1d1449ae75a938b1b4b0803d0b37d873a33e188
N_Onatskaya <Onatskaya.Nadejda@gmail.com>
2015-11-26 23:42:08 +0200

    Получение и покраска элемента по id работает
    А по содержимому, пока нет

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -1,6 +1,13 @@
 //add your code here
-setTimeout(function()
+setInterval(function()
 {
-	var myString = document.getElementById("aleks-krivtsov-row");
-	var myStringClass = myString.className += ' success';
-}, 8000 );
+	 var myString = document.getElementById("aleks-krivtsov-row");
+	var myStringClass = myString.className += ' success'; 
+	/* var onatskayaString = document.getElementById("nadejda-onatskaya-row");
+	onatskayaString.className += ' success'; */
+	var onatskayaString = document.getElementsByTagName("tr");
+	console.log(onatskayaString.firstChild().innerHTML);
+	if(onatskayaString.innerHTML === "https://github.com/Onatskaya") {
+		onatskayaString.className += ' success';
+	}
+}, 2000 );
