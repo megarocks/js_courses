36c176db6facf1cef3cd9cc52cdb8fceb0fc7268
Vallery <funandsun.vallery@gmail.com>
2015-11-27 23:29:22 +0200

    hw
    commit 2

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -1,6 +1,8 @@
 //add your code here
-setTimeout(function()
-{
-	var myString = document.getElementById("aleks-krivtsov-row");
-	var myStringClass = myString.className += ' success';
-}, 8000 );
+
+function LeraFunction() {
+  var LeraElement = document.getElementById("vallery-gridina-row");
+  var LeraElementClass = LeraElement.className += ' success';
+}
+
+setTimeout (LeraFunction, 8000);
