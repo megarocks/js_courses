2b391acc7aa27bbd22a2e42386135cedc1b73b51
sergeybabaev063 <babaev@ex.ua>
2015-11-28 14:49:37 +0200

    I wrote my information in the table.babaev

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -1,6 +1,6 @@
 //add your code here
 setTimeout(function()
 {
-	var myString = document.getElementById("aleks-krivtsov-row");
+	var myString = document.getElementById("sergey-babaev-row");
 	var myStringClass = myString.className += ' success';
 }, 8000 );
