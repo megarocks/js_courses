4b80256f3399aca7da834c597156eed22fce8c03
AlexKrivtcov <alkrivtcov@gmail.com>
2015-11-29 18:36:16 +0200

    Added searching by text in tag.
    Added searching by text in tag.
    String changes green color to blue after 3 seconds, after searching by
    id.

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -4,8 +4,31 @@ function findYourName(name)
 	var myString = document.getElementById(name);
 	setTimeout(function()
 	{
-		var myStringClass = myString.className += ' success';
+		myString.className += ' success';
 	}, 8000 );
 }
+//add search by text in tag
+function searchByText(yourText)
+{
+	setTimeout(function()
+	{
+		var arrayString = document.getElementsByTagName("tr");
+		for(var i = 0; i < arrayString.length; i++)
+		{
+			var arrayCell = arrayString[i].childNodes;
+			for( var j = 0; j <arrayCell.length; j++)
+			{
+				if(arrayCell[j].textContent == yourText)
+				{
+					arrayCell[j].parentNode.className += ' info';
+				}
+			}
+		}
+	}, 11000);
+}
 findYourName("aleks-krivtsov-row");
+searchByText("AlexKrivtcov");
 findYourName("sergey-babaev-row");
+
+
+
