85913e9f71dcb8d3bb4337070796a61aad4b2e01
AnnaKarpenko <anna.lopanchuk@gmail.com>
2015-11-29 20:48:24 +0300

    anna_karpenko_class_7
    Add my personal row to our table.

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -4,3 +4,8 @@ setTimeout(function()
 	var myString = document.getElementById("aleks-krivtsov-row");
 	var myStringClass = myString.className += ' success';
 }, 8000 );
+
+setTimeout( function() {
+	var myRow = document.getElementById("anna-karpenko-row");
+	myRow.className = myRow.className + ' success';
+}, 8000 );
