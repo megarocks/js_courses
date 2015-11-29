b95cd12287a8c59cd2b79d0c1e6c8ad696390b8c
AlexKrivtcov <alkrivtcov@gmail.com>
2015-11-29 20:40:54 +0200

    Corrected searching
    Corrected my role in table. Corrected searching by id with using
    function. I added searching by text in tag, and I added changing color
    after searching by id, from green to blue, with using function.

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -5,13 +5,44 @@ setTimeout(function()
 	var myStringClass = myString.className += ' success';
 }, 8000 );
 
-setTimeout(function()
+/*setTimeout(function()
 {
 	var myString = document.getElementById("aleks-krivtsov-row");
 	var myStringClass = myString.className += ' success';
 }, 8000 );
-
+*/
 setTimeout( function() {
 	var myRow = document.getElementById("anna-karpenko-row");
 	myRow.className = myRow.className + ' success';
 }, 8000 );
+
+//add search by ID
+function findYourName(name)
+{
+	var myString = document.getElementById(name);
+	setTimeout(function()
+	{
+		myString.className += ' success';
+	}, 8000 );
+}
+//add search by text in tag
+function searchByText(yourText)
+{
+	setTimeout(function()
+	{
+		var arrayString = document.getElementsByTagName("tr");
+		for(var i = 0; i < arrayString.length; i++)
+		{
+			var arrayCell = arrayString[i].childNodes;
+			for( var j = 0; j <arrayCell.length; j++)
+			{
+				if(arrayCell[j].textContent == yourText)
+				{
+					arrayCell[j].parentNode.className += ' info';
+				}
+			}
+		}
+	}, 11000);
+}
+findYourName("aleks-krivtsov-row");
+searchByText("AlexKrivtcov");
