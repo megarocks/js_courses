b21ace5d8d552f30b6ba60bcd2b48ace97da3215
AlexKrivtcov <alkrivtcov@gmail.com>
2015-11-29 20:46:27 +0200

    Corrected texta
    Corrected texta

--- class_07/commonWork.js
+++ class_07/commonWork.js
@@ -9,8 +9,8 @@ setTimeout(function()
 {
 	var myString = document.getElementById("aleks-krivtsov-row");
 	var myStringClass = myString.className += ' success';
-}, 8000 );
-*/
+}, 8000 );*/
+
 setTimeout( function() {
 	var myRow = document.getElementById("anna-karpenko-row");
 	myRow.className = myRow.className + ' success';
