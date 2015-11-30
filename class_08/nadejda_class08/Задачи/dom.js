c67b2384424d51a59b8cac0408ef8369137b2fe3
N_Onatskaya <Onatskaya.Nadejda@gmail.com>
2015-11-30 20:24:42 +0200

    files from class08 added

---
+++ class_08/nadejda_class08/Задачи/dom.js
@@ -0,0 +1,16 @@
+function getInputValues() {
+	var firstInputValue = dovument.getElementById("firstInput");
+
+	if (firstInputValue != Number){
+		alert("You didn't write a number");
+	} else {
+		firstInputValue = parseInt(firstInputValue.innerText);
+		console.log("firstInputValue");
+}
+
+
+
+
+function compareInputValues() {
+
+}
