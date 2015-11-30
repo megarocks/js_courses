363e9b5cda48363c9428a66e8788fb8090950270
N_Onatskaya <Onatskaya.Nadejda@gmail.com>
2015-11-30 23:44:19 +0200

    cat constructor is working now, work on compare input values has been started

--- class_08/nadejda_class08/Задачи/dom.js
+++ class_08/nadejda_class08/Задачи/dom.js
@@ -1,16 +1,30 @@
 function getInputValues() {
-	var firstInputValue = dovument.getElementById("firstInput");
+	var firstInputValue = document.getElementById("firstInput");
+	var secondInputValue = document.getElementById("secondInput");
 
 	if (firstInputValue != Number){
 		alert("You didn't write a number");
 	} else {
 		firstInputValue = parseInt(firstInputValue.innerText);
+		secondInputValue = parseInt(secondInputValue.innerText);
 		console.log("firstInputValue");
-}
 
+		var inputValuesArray = [firstInputValue, secondInputValue]
+		return inputValuesArray
+	}
+};
 
 
 
 function compareInputValues() {
+	var inputValues = getInputValues();
+	if (inputValues[0] > inputValues[1]) {
+		console.log(inputValues[0]);
+	} else if (inputValues[0] < inputValues[1]) {
+		console.log(inputValues[1]);
+	} else {
+		console.log("The numbers are equal");
+	}
+};
 
-}
+compareInputValues();
