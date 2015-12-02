5595696fb2aefba7b01c1379c66584664c765020
Onatskaya <Onatskaya.Nadejda@gmail.com>
2015-12-02 14:37:16 +0200

    Comparing and calculating of input values is working

--- class_08/nadejda_class08/Задачи/dom.js
+++ class_08/nadejda_class08/Задачи/dom.js
@@ -1,25 +1,55 @@
+var firstInputValue;
+var secondInputValue;
+
 function getInputValues() {
-	var firstInputValue = document.getElementById("firstInput").value;
-	var secondInputValue = document.getElementById("secondInput").value;
+	
+	firstInputValue = parseInt(document.getElementById("firstInput").value);
+	secondInputValue = parseInt(document.getElementById("secondInput").value);
 
-	firstInputValue = parseInt(firstInputValue);
-	secondInputValue = parseInt(secondInputValue);
-
-		var inputValuesArray = [firstInputValue, secondInputValue]
-		return inputValuesArray
-	};
+	var inputValuesArray = [firstInputValue, secondInputValue]
+	return inputValuesArray
+};
 
 
 
 function compareInputValues() {
 	var inputValues = getInputValues();
+	var result = document.createElement('div');
+	document.getElementById("inputs").appendChild(result);
+	  
+
 	if (inputValues[0] > inputValues[1]) {
-		console.log("The first input number is bigger (", inputValues[0], ")");
+		result.innerText = "The first input number is bigger (" + inputValues[0] + ")";
 	} else if (inputValues[0] < inputValues[1]) {
-		console.log("The second input number is bigger (", inputValues[1], ")");
+		result.innerText = "The second input number is bigger (" + inputValues[1] + ")";
 	} else {
-		console.log("The numbers are equal");
+		result.innerText = "The numbers are equal";
 	}
 };
 
-compareInputValues();
+function calculateInputValues() {
+	firstInputValue = parseInt(document.getElementById("firstInput").value);
+	secondInputValue = parseInt(document.getElementById("secondInput").value);
+
+	var selectValue = document.getElementById("operators").value;
+	var result;
+	switch(selectValue) {
+		case "+": 
+			result = firstInputValue + secondInputValue
+		break
+		case "-":
+			result = firstInputValue - secondInputValue
+		break
+		case "*":
+			result = firstInputValue * secondInputValue
+		break
+		case "/":
+			result = firstInputValue / secondInputValue
+		break
+
+	}
+	var resultInfo = document.createElement('div');
+	resultInfo.innerText = "The result is: " + firstInputValue + selectValue + secondInputValue + " = " + result;
+	document.getElementById("selectAria").appendChild(resultInfo);
+};
+
