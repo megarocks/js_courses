8fa59f1a7f84ec1e00d953b33e58292d35704cde
N_Onatskaya <Onatskaya.Nadejda@gmail.com>
2015-12-03 13:01:30 +0200

    Table for showing data of math operations is ready

--- class_08/nadejda_class08/Задачи/dom.js
+++ class_08/nadejda_class08/Задачи/dom.js
@@ -1,83 +1,132 @@
 var firstInputValue;
 var secondInputValue;
+var selectValue;
+var results;
 
+//Получение значения из инпутов и селекта
 function getInputValues() {
 	
 	firstInputValue = parseInt(document.getElementById("firstInput").value);
 	secondInputValue = parseInt(document.getElementById("secondInput").value);
+	selectValue = document.getElementById("operators").value;
 
-	var inputValuesArray = [firstInputValue, secondInputValue]
+	var inputValuesArray = {
+		firstValue: firstInputValue,
+		secondValue:  secondInputValue,
+		operator: selectValue
+	}
 	return inputValuesArray
 };
 
 
-
+// Функция сравнения данных, полученых в предидущей функции
 function compareInputValues() {
 	var inputValues = getInputValues();
 	var result = document.createElement('div');
 	document.getElementById("inputs").appendChild(result);
 	  
 
-	if (inputValues[0] > inputValues[1]) {
-		result.innerText = "The first input number is bigger (" + inputValues[0] + ")";
-	} else if (inputValues[0] < inputValues[1]) {
-		result.innerText = "The second input number is bigger (" + inputValues[1] + ")";
+	if (inputValues.firstValue > inputValues.secondValue) {
+		result.innerText = "The first input number is bigger (" + inputValues.firstValue + ")";
+	} else if (inputValues.firstValue < inputValues.secondValue) {
+		result.innerText = "The second input number is bigger (" + inputValues.secondValue + ")";
 	} else {
 		result.innerText = "The numbers are equal";
 	}
 };
 
+// Функция, считающая введенное пользователем выражение (значения из инпутов и знак из селекта)
 function calculateInputValues() {
-	firstInputValue = parseInt(document.getElementById("firstInput").value);
-	secondInputValue = parseInt(document.getElementById("secondInput").value);
+	var inputValues = getInputValues();
 
-	var selectValue = document.getElementById("operators").value;
-	var result;
-	switch(selectValue) {
+
+	switch(inputValues.operator) {
 		case "+": 
-			result = firstInputValue + secondInputValue
+			results = inputValues.firstValue + inputValues.secondValue
 		break
 		case "-":
-			result = firstInputValue - secondInputValue
+			results = inputValues.firstValue - inputValues.secondValue
 		break
 		case "*":
-			result = firstInputValue * secondInputValue
+			results = inputValues.firstValue * inputValues.secondValue
 		break
 		case "/":
-			result = firstInputValue / secondInputValue
+			results = inputValues.firstValue / inputValues.secondValue
 		break
 
 	}
 	var resultInfo = document.createElement('div');
-	resultInfo.innerText = "The result is: " + firstInputValue + selectValue + secondInputValue + " = " + result;
+	resultInfo.innerText = "The result is: " + inputValues.firstValue + inputValues.operator + inputValues.secondValue + " = " + results;
 	document.getElementById("inputs").appendChild(resultInfo);
+	return results;
 
 };
 
+
+// Глобальная переменная для тела таблицы(чтоб таблица не переписывалась заново при каждом нажатии кнопки, а в нее добавлялись результаты)
+var tableBody = {};
+
 function createResultsTable() {
 
-	firstInputValue = parseInt(document.getElementById("firstInput").value);
-	secondInputValue = parseInt(document.getElementById("secondInput").value);
+	var inputValues = getInputValues();
+	
+	// Если таблица с таким классом не существует, тогда создаем такую таблицу
+	if(!document.getElementsByClassName('table-striped').length){
+	
+		var resultsTable = document.createElement("table");
+		resultsTable.border='1';
+		resultsTable.className = "table table-striped";
+		document.getElementById("resultsTable").appendChild(resultsTable);
 
-	var resultsTable = document.createElement("table");
-	resultsTable.border='1';
-	document.getElementById("resultsTable").appendChild(resultsTable);
+		// Добавляем шапку таблицы
+		var tableHead = document.createElement('thead');
+		resultsTable.appendChild(tableHead);
+		
+		var tr = document.createElement('tr');
+		tableHead.appendChild(tr);
 
-	var tableBody = document.createElement('tbody');
-    resultsTable.appendChild(tableBody);
-     
-    var tr = document.createElement('tr');
-    tableBody.appendChild(tr);
+		for (var i=0; i<3; i++){
+			var td = document.createElement('td');
+			tr.appendChild(td);
+		}
+		
+		document.getElementsByTagName('td')[0].innerText = "Date";
+		
+		document.getElementsByTagName('td')[1].innerText = "Expression";
+		
+		document.getElementsByTagName('td')[2].innerText = "Result";
+		
+		// Добавляем тело таблицы
+		tableBody = document.createElement('tbody');
+		tableBody.insertRow(0);
+		resultsTable.appendChild(tableBody);		
+	}
+	
+	// Добавляем одну строку в таблицу при каждом вызове функции и заполняем ее данными (время операции, вычисляемое выражение, результат)
+    var date = (new Date).getDay() + "." + (new Date).getMonth() + "." + (new Date).getFullYear() + ", " + (new Date).getHours() + "." + (new Date).getMinutes();
+	var tr = document.createElement('tr');
+		tableBody.appendChild(tr);
+		for (var j=0; j<3; j++){
+			var td = document.createElement('td');
+			tr.appendChild(td);
 
-    for (var i=0; i<4; i++){
-        var td = document.createElement('td');
-        td.width='150';
-        //td.appendChild(document.createTextNode("Ячейка"));
-        tr.appendChild(td);
-    }
-    /*var date = new Date;
-    document.getElementsByTagName('td').cellIndex[0].innerText = "Date";*/
-    /*var expression = firstInputValue + selectValue + secondInputValue;
-    var result*/
+			if (j == 0){
+				td.innerText = date;
+			} else if(j == 1){
+				td.innerText = inputValues.firstValue + inputValues.operator + inputValues.secondValue;
+		
+			} else if(j == 2){
+				td.innerText = calculateInputValues();
+		
+			}
+
+
+		}
+	
+    
 }
-createResultsTable();
+
+// Функция покраски строки с наибольшим результатом
+/*function painTheBiggestValueRow() {
+
+}*/
