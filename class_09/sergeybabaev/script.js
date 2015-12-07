363a5cb6d755d5b1a1ca6ac435d765cd021c1ff8
babaevsergey <sergeybabaev063@gmail.com>
2015-12-07 16:46:58 +0200

    homework

--- class_09/sergeybabaev/script.js
+++ class_09/sergeybabaev/script.js
@@ -1,21 +1,129 @@
+var getX;
+var getY;
+var sign;
+var results;
+var myArray = [];
+var buttonCount = $("#button_count").click(counting);
+var buttonMax = $("#buttonMax").click(findMax); 
+var buttonMin = $("#buttonMin").click(findMin); 
+var buttonSum = $("#buttonSum").click(calcSum);                    
+var buttonAvg = $("#buttonAvg").click(calcAvg);  // Спросить можно ли переменные называть так же как и айдишники
 
-function getOperator() {
-	var takeOperator = document.getElementById('chooseOperator'); // Получаю сюда значение сэлэкта.
-	var sign = takeOperator.value; // Получаю непосредственно знак.
+function getValue() {
+	getX = parseInt($('#ourX').val());
+	getY =  parseInt($('#ourY').val()); 
+	sign = $('#chooseOperator').val(); 
+};
 
+function counting() {
+	var value = getValue();
+
+	switch (sign) {
+		case "+": 
+			results = getX + getY
+		break
+		case "-":
+			results = getX - getY
+		break
+		case "*":
+			results = getX * getY
+		break
+		case "/":
+			results = getX / getY
+		break
+	}
+	myArray.push(results);
+	console.log(myArray);
+};
+
+
+function findMax() {
+	var max = Math.max.apply(Math,myArray);
+	console.log("Максимальное значение: ",max);
+};
+
+
+function findMin() {
+	var min = Math.min.apply(Math,myArray);
+	console.log("Минимальное значение: ",min);
+};
+
+function calcSum() {
+	var sum = 0;
+	for(var i=0; i < myArray.length; i++){
+	    sum = sum + parseInt(myArray[i]);
+	}
+	console.log("Сумма всех чисел: ",sum);
+};
+
+function calcAvg() {						// Спросить как сократить код в расчетах среднего
+	var sum = 0;
+	for(var i=0; i < myArray.length; i++){
+	    sum = sum + parseInt(myArray[i]);
+	}
+	var avg = sum/myArray.length;
+	console.log("Среднее всех чисел: ",avg);
+};
+
+// //----------------------------------------------------------------------------
+
+	var tableBody = {};
+
+	function createResultsTable() {
+
+	var values = getValue();
+	// Если таблица с таким классом не существует, тогда создаем такую таблицу
+	
+		var resultsTable = document.createElement("table");
+		resultsTable.border='1';
+		resultsTable.className = "table table-striped";
+		document.getElementById("resultsTable").appendChild(resultsTable);
+
+		// Добавляем шапку таблицы
+		var tableHead = document.createElement('thead');
+		resultsTable.appendChild(tableHead);
+		
+		var tr = document.createElement('tr');
+		tableHead.appendChild(tr);
+
+		for (var i=0; i<3; i++){
+			var td = document.createElement('td');
+			tr.appendChild(td);
+		}
+		
+		document.getElementsByTagName('td')[0].innerText = "Date";
+		
+		document.getElementsByTagName('td')[1].innerText = "Expression";
+		
+		document.getElementsByTagName('td')[2].innerText = "Result";
+		
+		// Добавляем тело таблицы
+		tableBody = document.createElement('tbody');
+		tableBody.insertRow(0);
+		resultsTable.appendChild(tableBody);		
+	
+	
+	// Добавляем одну строку в таблицу при каждом вызове функции и заполняем ее данными (время операции, вычисляемое выражение, результат)
+    
+	var date = moment().format('LLLL');
+	var tr = document.createElement('tr');
+		tableBody.appendChild(tr);
+		for (var j=0; j<3; j++){
+			var td = document.createElement('td');
+			tr.appendChild(td);
+
+			if (j == 0){
+				td.innerText = date;
+			} else if(j == 1){
+				td.innerText = inputValues.firstValue + inputValues.operator + inputValues.secondValue;
+		
+			} else if(j == 2){
+				td.innerText = calculateInputValues();
+		
+			}
+
+
+		}
+	
+    
 }
-
-function count() {
-	var getX = document.getElementById('ourX'); // Получаю сюда x
-	var getY = document.getElementById('ourY'); // Получаю сюда y
-	var x = getX.value; // Забираю отсюда именно цифру 
-	var y = getY.value; // Забираю отсюда именно цифру
-	console.info("Это икс",x,"Это игрик",y); // Для себя
-
-
-	// Есть цифры, есть знак. Пора писать swith. Но я не могу получить результат из getOperator. 
-	// С помощью return пробывал, у меня не получилось. Что то пишу не так. 
-}
-
-
-
